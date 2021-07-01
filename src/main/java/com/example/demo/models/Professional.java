ed15e50f3d938e7f68d8838d7753a95c464523e7
kfiddle <kenjfiddle@gmail.com>
2021-07-01 16:49:11 -0400

    more modeling

--- src/main/java/com/example/demo/models/Professional.java
+++ src/main/java/com/example/demo/models/Professional.java
@@ -24,7 +24,6 @@ public class Professional extends User {
     private boolean volunteerInd;
 
 
-
     public Professional() {
     }
 
@@ -32,5 +31,83 @@ public class Professional extends User {
         super(email, firstName, lastName, usaState, dateCreated);
     }
 
+    public Professional(String email, String firstName, String lastName, USAState usaState, LocalDate dateCreated, boolean approvedByAdmin, String careerTitle, String careerDescription, String educationDescription, String jobBestDescription, String jobWorstDescription, String pastChangeDesc, String futureChangeDesc, AccountType accountType, String company, String university, boolean gameHelpInd, boolean gameFundingInd, boolean socialMediaInd, boolean volunteerInd) {
+        super(email, firstName, lastName, usaState, dateCreated);
+        this.approvedByAdmin = approvedByAdmin;
+        this.careerTitle = careerTitle;
+        this.careerDescription = careerDescription;
+        this.educationDescription = educationDescription;
+        this.jobBestDescription = jobBestDescription;
+        this.jobWorstDescription = jobWorstDescription;
+        this.pastChangeDesc = pastChangeDesc;
+        this.futureChangeDesc = futureChangeDesc;
+        this.accountType = accountType;
+        this.company = company;
+        this.university = university;
+        this.gameHelpInd = gameHelpInd;
+        this.gameFundingInd = gameFundingInd;
+        this.socialMediaInd = socialMediaInd;
+        this.volunteerInd = volunteerInd;
+    }
 
+
+    public boolean isApprovedByAdmin() {
+        return approvedByAdmin;
+    }
+
+    public String getCareerTitle() {
+        return careerTitle;
+    }
+
+    public String getCareerDescription() {
+        return careerDescription;
+    }
+
+    public String getEducationDescription() {
+        return educationDescription;
+    }
+
+    public String getJobBestDescription() {
+        return jobBestDescription;
+    }
+
+    public String getJobWorstDescription() {
+        return jobWorstDescription;
+    }
+
+    public String getPastChangeDesc() {
+        return pastChangeDesc;
+    }
+
+    public String getFutureChangeDesc() {
+        return futureChangeDesc;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public boolean isGameHelpInd() {
+        return gameHelpInd;
+    }
+
+    public boolean isGameFundingInd() {
+        return gameFundingInd;
+    }
+
+    public boolean isSocialMediaInd() {
+        return socialMediaInd;
+    }
+
+    public boolean isVolunteerInd() {
+        return volunteerInd;
+    }
 }
